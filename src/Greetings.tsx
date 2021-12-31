c250231ea770fb8a176004d53bcb51f758d1e02d
srini <srinivasarajui@gmail.com>
2022-01-01 00:23:49 +0530

    Fixed webpack issues

--- src/Greetings.tsx
+++ src/Greetings.tsx
@@ -3,6 +3,7 @@ import {ColoredText, InnerContainer} from './components';
 export const Greetings: React.FC = () => {
 	return (
 		<InnerContainer>
+			<ColoredText>Dear Amma</ColoredText>
 			<ColoredText>Happy New Year</ColoredText>
 			<ColoredText>2022</ColoredText>
 		</InnerContainer>
--- src/Video.tsx
+++ src/Video.tsx
@@ -1,13 +1,60 @@
+import {Audio} from 'remotion';
+import {Card} from './Card';
 import {Composition} from 'remotion';
-import {MyComposition} from './Composition';
-import {totalDuration} from './sequence';
+import {GitRepo} from './GitRepo';
+import {GreetingFrom} from './GreetingFrom';
+import {Greetings} from './Greetings';
+import {Message} from './Message';
+import {Sequence} from 'remotion';
+import {Transition} from './Transition';
+import audio from './bg.mp3';
+import {bgColor1} from './components';
 
 export const RemotionVideo: React.FC = () => {
+	const seq = [
+		{comp: <Greetings />, duration: 120},
+		{comp: <Message />, duration: 300},
+		{comp: <GreetingFrom />, duration: 180},
+		{comp: <GitRepo />, duration: 180},
+	];
+	const totalDuration = seq.reduce((acc, item) => acc + item.duration, 0);
+	let previousEnd = 0;
+	const seqItems = seq
+		.map((item) => {
+			const start = previousEnd;
+			previousEnd += item.duration;
+			return {
+				comp: item.comp,
+				duration: item.duration,
+				start,
+			};
+		})
+		.map((item, index) => (
+			<Sequence
+				key={index}
+				from={item.start}
+				durationInFrames={item.duration + 8}
+			>
+				<Transition type="in">
+					<Card>{item.comp}</Card>
+				</Transition>
+			</Sequence>
+		));
+	const CurrentComposition = () => {
+		return (
+			<div style={{flex: 1, backgroundColor: bgColor1}}>
+				<div>
+					{seqItems}
+					<Audio src={audio} />
+				</div>
+			</div>
+		);
+	};
 	return (
 		<>
 			<Composition
 				id="HappyNewYear"
-				component={MyComposition}
+				component={CurrentComposition}
 				durationInFrames={totalDuration}
 				fps={30}
 				width={1280}
