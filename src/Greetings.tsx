38b577329770d41144defe2abc07d75cda196cf9
srini <srinivasarajui@gmail.com>
2022-01-01 00:24:38 +0530

    Removed user name

--- src/Greetings.tsx
+++ src/Greetings.tsx
@@ -3,7 +3,6 @@ import {ColoredText, InnerContainer} from './components';
 export const Greetings: React.FC = () => {
 	return (
 		<InnerContainer>
-			<ColoredText>Dear Amma</ColoredText>
 			<ColoredText>Happy New Year</ColoredText>
 			<ColoredText>2022</ColoredText>
 		</InnerContainer>
