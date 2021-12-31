76cf11ccf5da9520eee6c971f87b6fef03e0a7d8
srini <srinivasarajui@gmail.com>
2022-01-01 01:17:48 +0530

    Added Name variable

--- src/Greetings.tsx
+++ src/Greetings.tsx
@@ -1,8 +1,10 @@
 import {ColoredText, InnerContainer} from './components';
 
-export const Greetings: React.FC = () => {
+export const Greetings: React.FC<{name: string}> = ({name}) => {
 	return (
 		<InnerContainer>
+			<ColoredText>Dear</ColoredText>
+			<ColoredText>{name}</ColoredText>
 			<ColoredText>Happy New Year</ColoredText>
 			<ColoredText>2022</ColoredText>
 		</InnerContainer>
--- src/Video.tsx
+++ src/Video.tsx
@@ -12,35 +12,36 @@ import {bgColor1} from './components';
 
 export const RemotionVideo: React.FC = () => {
 	const seq = [
-		{comp: <Greetings />, duration: 120},
+		{comp: <Greetings name="todo" />, duration: 120},
 		{comp: <Message />, duration: 300},
 		{comp: <GreetingFrom />, duration: 180},
 		{comp: <GitRepo />, duration: 180},
 	];
 	const totalDuration = seq.reduce((acc, item) => acc + item.duration, 0);
-	let previousEnd = 0;
-	const seqItems = seq
-		.map((item) => {
-			const start = previousEnd;
-			previousEnd += item.duration;
-			return {
-				comp: item.comp,
-				duration: item.duration,
-				start,
-			};
-		})
-		.map((item, index) => (
-			<Sequence
-				key={index}
-				from={item.start}
-				durationInFrames={item.duration + 8}
-			>
-				<Transition type="in">
-					<Card>{item.comp}</Card>
-				</Transition>
-			</Sequence>
-		));
-	const CurrentComposition = () => {
+
+	const CurrentComposition: React.FC<{name: string}> = ({name}) => {
+		let previousEnd = 0;
+		const seqItems = seq
+			.map((item) => {
+				const start = previousEnd;
+				previousEnd += item.duration;
+				return {
+					comp: item.comp,
+					duration: item.duration,
+					start,
+				};
+			})
+			.map((item, index) => (
+				<Sequence
+					key={index}
+					from={item.start}
+					durationInFrames={item.duration + 8}
+				>
+					<Transition type="in">
+						<Card>{index !== 0 ? item.comp : <Greetings name={name} />}</Card>
+					</Transition>
+				</Sequence>
+			));
 		return (
 			<div style={{flex: 1, backgroundColor: bgColor1}}>
 				<div>
@@ -59,6 +60,7 @@ export const RemotionVideo: React.FC = () => {
 				fps={30}
 				width={1280}
 				height={720}
+				defaultProps={{name: 'Amma'}}
 			/>
 		</>
 	);
